c6e23e800d0c229a6f813c9293b389140536fa89
sif <sifddin@hotmail.cmo>
2023-06-30 22:29:14 +0200

    Merge unrelated histories

--- utils/stopTimer.js
+++ utils/stopTimer.js
@@ -1,9 +1,27 @@
+//  _____ _                _           _   _              
+// /  ___| |              | |         | | | |             
+// \ `--.| |_ ___  _ __   | |__  _   _| |_| |_ ___  _ __  
+//  `--. \ __/ _ \| '_ \  | '_ \| | | | __| __/ _ \| '_ \ 
+// /\__/ / || (_) | |_) | | |_) | |_| | |_| || (_) | | | |
+// \____/ \__\___/| .__/  |_.__/ \__,_|\__|\__\___/|_| |_|
+//                | |                                     
+//                |_|                              
+
+
+/*
+* @type {HTMLButtonElement}
+**/
+
 const stopButtonEl = document.getElementById("stop");
 
+
+// Stops the timer and enables the start button 
 function stopTimer() {
     clearInterval(timerInterval);
     startButtonEl.disabled = false;
     stopButtonEl.disabled = true;
   }
 
+
+  // Event listener for the stop button
   stopButtonEl.addEventListener("click", stopTimer);
--- utils/timer.js
+++ utils/timer.js
@@ -1,10 +1,12 @@
 const timerEl = document.getElementById("timer");
 
 function formatTime(elapsedTime) {
-   
+    // ==================================================================       
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
     const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
     const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    //===================================================================
+
     return (
       (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
       ":" +
